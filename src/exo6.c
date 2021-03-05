f4e8de7661c9382da70b1ab81d2a33643d992826
rosalieperee <roseallie92@gmail.com>
2021-03-05 17:20:50 +0100

    adding 6

--- src/exo6.c
+++ src/exo6.c
@@ -25,42 +25,40 @@
 int main(void) {	
 	float X;
 
-	//
+	//opérateur bit à bit AND
 	printf("5 & 6 : ");
 	X = 5 & 6;
 	printf("%f", X); 
 
-	//
+	//opérateur logique AND
 	printf("\n5 && 6 : ");
 	X = 5 && 6;
 	printf("%f", X); 
 
-	//
+	//opérateur bit à bit OR
 	printf("\n5 | 6 : ");
 	X = 5 | 6;
 	printf("%f", X); 
 
-	//
+	//opérateur logique OR
 	printf("\n5 || 6 : ");
 	X = 5 || 6;
 	printf("%f", X); 
 
-	//
+	//opérateur bi à bit XOR
 	printf("\n5 ^ 6 : ");
 	X = 5 ^ 6;
 	printf("%f", X); 
 
-	//
-	printf("\n5 ^ 6 : ");
-	X = 5 ^^ 6;
-	printf("%f", X); 
+	printf("\n5 ^^ 6 : ");
+	printf("cet operateur n'est pas un operateur valable en C"); 
 
-	//
+	//opérateur bit a bit not
 	printf("\n~5 : ");
 	X = ~5;
 	printf("%f", X); 
 
-	//
+	//opérateur logique not
 	printf("\n!5 : ");
 	X = !5;
 	printf("%f", X); 
