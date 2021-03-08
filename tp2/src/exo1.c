1c2ca546349bb7d30259f4b36fcdf4afca3ee6b4
Nerze <y.cottin@hotmail.fr>
2021-03-08 18:33:25 +0100

    TP2 - Ex 1 et 2-2

---
+++ tp2/src/exo1.c
@@ -0,0 +1,112 @@
+/*
+ * exo1.c
+ *
+ *  Created on: 8 mars 2021
+ *      Author: Cottin Yohan
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void square(int X){
+	for (int Y=0;Y<X;Y=Y+1){
+		for(int i=0;i<X;i=i+1){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+
+void rightTri(int X){
+	for (int Y=0;Y<X;Y=Y+1){
+		for(int i=0; i<=Y;i=i+1){
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (int Y=1;Y<X;Y=Y+1){
+			for(int i=0; i<X-Y;i=i+1){
+				printf("*");
+			}
+			printf("\n");
+		}
+}
+
+void leftTri(int X){
+	for (int Y=1;Y<=X;Y=Y+1){
+		for(int i=0; i<X-Y;i=i+1){
+			printf(" ");
+		}
+		for(int i=0; i<Y;i=i+1){
+					printf("*");
+				}
+		printf("\n");
+	}
+	for (int Y=1;Y<X;Y=Y+1){
+		for(int i=0; i<Y;i=i+1){
+			printf(" ");
+		}
+		for(int i=0; i<X-Y;i=i+1){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+
+void upTri(int X){
+	for (int Y=1;Y<=X;Y=Y+1){
+		for(int i=0; i<X-Y;i=i+1){
+			printf(" ");
+		}
+		for(int i=1; i<Y*2;i=i+1){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+
+void diamond(int X){
+	upTri(X);
+	for(int Y=1;Y<X;Y=Y+1){
+		for(int i=0;i<Y;i=i+1){
+			printf(" ");
+		}
+		for(int i=1;i<(X-Y)*2;i=i+1){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
+
+int main(void) {
+	int X=0;
+	while(X<1){
+		printf("Enter a number :\n");
+		scanf("%i",&X);
+	}
+	int shape=0;
+	while(shape>5 || shape<1){
+		printf("Choose a shape :\n");
+		scanf("%i",&shape);
+	}
+	switch(shape){
+	case 1:
+		square(X);
+		break;
+	case 2:
+		rightTri(X);
+		break;
+	case 3:
+		leftTri(X);
+		break;
+	case 4:
+		upTri(X);
+		break;
+	case 5:
+		diamond(X);
+		break;
+	}
+	return EXIT_SUCCESS;
+}
+
+
