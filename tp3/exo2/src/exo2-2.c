be4b08eeb03bcb1b91511102f28277d90e3a6bd0
rosalieperee <roseallie92@gmail.com>
2021-03-11 11:15:01 +0100

    cleaning up and uploading tp3/exo2

---
+++ tp3/exo2/src/exo2-2.c
@@ -0,0 +1,31 @@
+/*
+ ============================================================================
+ Name        : exo2-1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Ecrire un programme affiche «Hello world» à l’aide d’une 
+ 			fonction affiche_hello() en
+ 			1.Définissant l’entête et le corps de la fonction avant le main
+ 			2.Définissant l’entête de la fonction avant le main et le corps 
+ 				de la fonction après le main
+ 			3.Définissant l’entête et le corps de la fonction dans un 
+ 				fichier fonction.h
+ 			4.Définissant  l’entête  de  la  fonction  dans  un  fichier 
+ 				fonction.h et  le  corps  de  la  fonction  dans  un fichier 
+ 				fonction.c
+ ============================================================================
+ */ 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void affiche_hello();
+
+int main(){
+	affiche_hello();
+}
+
+void affiche_hello(){
+	printf("%s\n", "Hello world");
+}
---
+++ tp3/exo2/src/exo2-3.c
@@ -0,0 +1,27 @@
+/*
+ ============================================================================
+ Name        : exo2-1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Ecrire un programme affiche «Hello world» à l’aide d’une 
+ 			fonction affiche_hello() en
+ 			1.Définissant l’entête et le corps de la fonction avant le main
+ 			2.Définissant l’entête de la fonction avant le main et le corps 
+ 				de la fonction après le main
+ 			3.Définissant l’entête et le corps de la fonction dans un 
+ 				fichier fonction.h
+ 			4.Définissant  l’entête  de  la  fonction  dans  un  fichier 
+ 				fonction.h et  le  corps  de  la  fonction  dans  un fichier 
+ 				fonction.c
+ ============================================================================
+ */ 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "exo2-3.h"
+
+int main(){
+	affiche_hello();
+}
+
